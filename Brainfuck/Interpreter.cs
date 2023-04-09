d021f43939bf5ef679b551bb0c9746fcd68a77e7
kooooala <111371267+kooooala@users.noreply.github.com>
2023-04-09 21:37:09 +0100

    Interpreter works now

---
+++ Brainfuck/Interpreter.cs
@@ -0,0 +1,104 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+
+namespace Brainfuck;
+
+public class Interpreter
+{
+    private List<byte> _cells { get; set; } = new() { 0 };
+    private int _index = 0;
+
+    public void Execute(string code)
+    {
+        
+        
+        for (int i = 0; i < code.Length; i++)
+        {
+            var c = code[i];
+            switch (c)
+            {
+                case '>':
+                    _index++;
+                    if (_index >= _cells.Count) _cells.Add(0);
+                    break;
+                case '<':
+                    if (_index - 1 < 0)
+                    {
+                        Error("pointer out of bound", i + 1);
+                        return;
+                    }
+                    _index--;
+                    break;
+                case '+':
+                    _cells[_index]++;
+                    break;
+                case '-':
+                    _cells[_index]--;
+                    break;
+                case '.':
+                    Console.Write((char)_cells[_index]);
+                    break;
+                case ',':
+                    _cells[_index] = Encoding.Default.GetBytes(Console.ReadKey().KeyChar.ToString())[0];
+                    break;
+                case '[':
+                    if (_cells[_index] == 0)
+                    {
+                        int loops = 1;
+                        char currentChar;
+
+                        while (loops > 0)
+                        {
+                            i++;
+                            if (i >= code.Length)
+                            {
+                                Error("missing ']'", null);
+                                return;
+                            }
+                                
+                            currentChar = code[i];
+                                
+                            if (currentChar == '[') loops++;
+                            else if (currentChar == ']') loops--;
+                        }
+                    }
+                    
+                    break;
+                case ']':
+                    if (_cells[_index] != 0)
+                    {
+                        int loops = 1;
+                        char currentChar;
+
+                        while (loops > 0)
+                        {
+                            i--;
+                            if (i < 0)
+                            {
+                                Error("missing '['", null);
+                                return;
+                            }
+                                
+                            currentChar = code[i];
+
+                            if (currentChar == '[') loops--;
+                            else if (currentChar == ']') loops++;
+                        }
+                    }
+
+                    break;
+
+                default:
+                    continue;
+            }
+        }
+    }
+
+    private static void Error(string message, int? index)
+    {
+        if (index is not null)
+            Console.WriteLine($"Error occured at char {index}: {message}");
+        else 
+            Console.WriteLine($"Error occured: {message}");
+    }
+}
---
+++ Brainfuck/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Brainfuck;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            var interpreter = new Interpreter();
+            while (true)
+            {
+                Console.Write("> ");
+                var code = Console.ReadLine();
+                if (code == null) continue;
+                
+                interpreter.Execute(code);
+            }
+        }
+        else if (args.Length == 1)
+        {
+            var code = File.ReadAllText(args[1]);
+            var interpreter = new Interpreter();
+            
+            interpreter.Execute(code);
+        }
+        else Console.WriteLine("Usage: brainfuck [file]");
+    }
+}
