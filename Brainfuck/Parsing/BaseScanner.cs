b9cdfb6f7460d7ea65786cdcdc5d24657d15b903
kooooala <omgthereisaleaf@gmail.com>
2023-04-22 01:06:19 +0100

    Extracted an abstract scanner class from the parser

---
+++ Brainfuck/Parsing/BaseScanner.cs
@@ -0,0 +1,30 @@
+﻿namespace Brainfuck.Parsing;
+
+public abstract class BaseScanner<TInput, TOutput>
+{
+    protected List<TInput> Inputs;
+    protected List<TOutput> Outputs;
+    protected int CurrentLocation = 0;
+    
+    protected BaseScanner(List<TInput> inputs)
+    {
+        Inputs = inputs;
+        Outputs = new();
+    }
+    
+    protected virtual TInput GetCurrent() => Inputs[CurrentLocation];
+    
+    protected virtual void Move()
+    {
+        CurrentLocation++;
+    }
+    
+    protected virtual void Move(int count)
+    {
+        CurrentLocation += count;
+    }
+
+    protected virtual TInput Next() => Inputs[CurrentLocation + 1];
+
+    protected virtual TInput Next(int count) => Inputs[CurrentLocation + count];
+}
